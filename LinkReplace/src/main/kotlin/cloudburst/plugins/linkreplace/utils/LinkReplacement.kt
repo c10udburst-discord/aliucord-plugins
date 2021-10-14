c7fecb6b29de80d10973a276ba3aeff3a20b45d6
C10udburst <18114966+C10udburst@users.noreply.github.com>
2021-10-14 21:02:39 +0200

    :trollface:

--- LinkReplace/src/main/kotlin/cloudburst/plugins/linkreplace/utils/LinkReplacement.kt
+++ LinkReplace/src/main/kotlin/cloudburst/plugins/linkreplace/utils/LinkReplacement.kt
@@ -7,7 +7,8 @@ data class LinkReplacement(var fromText: String, var toDomain: String) {
     companion object {
         val DEFAULT_LIST = arrayOf(
             LinkReplacement("((www|m)\\.)?(youtube\\.com|youtu.be)", "invidious-us.kavin.rocks"),
-            LinkReplacement("((www|m)\\.)?twitter\\.com", "nitter.net")
+            LinkReplacement("((www|m)\\.)?twitter\\.com", "nitter.net"),
+            LinkReplacement("bluesmods\\.com", "aliucord.tk")
         )
     }
 }
