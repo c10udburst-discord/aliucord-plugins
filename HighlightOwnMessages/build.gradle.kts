4d1394a2b069d29bfc521cf72b40cb012497320b
C10udburst <18114966+C10udburst@users.noreply.github.com>
2022-02-04 11:47:19 +0100

    updated to the newest discord version

--- HighlightOwnMessages/build.gradle.kts
+++ HighlightOwnMessages/build.gradle.kts
@@ -1,2 +1,2 @@
-version = "1.1.1"
+version = "1.1.2"
 description = "Customize how messages from yourself look."
--- SendEmbeds/build.gradle.kts
+++ SendEmbeds/build.gradle.kts
@@ -1,2 +1,2 @@
-version = "1.2.6"
+version = "1.2.7"
 description = "Send embed messages with /embed or with a button."
