0ebb0b865fe6e5d130204231eeabb860fb88d76f
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2022-04-09 09:08:45 +0200

    remove default because people cope cope cope

--- TextReplace/src/main/kotlin/cloudburst/plugins/textreplace/utils/TextReplacement.kt
+++ TextReplace/src/main/kotlin/cloudburst/plugins/textreplace/utils/TextReplacement.kt
@@ -27,9 +27,7 @@ data class TextReplacement(
     companion object {
         val DEFAULT_LIST = arrayOf(
             TextReplacement("media.discordapp.net", "cdn.discordapp.com", false, true, true, false, false),
-            TextReplacement("((www|m)\\.)?(youtube\\.com|youtu.be)", "invidious-us.kavin.rocks", true, true, true, true, true),
-            TextReplacement("((www|m)\\.)?twitter\\.com", "nitter.net", true, true, true, true, true),
-            TextReplacement("bluesmods.com", "aliucord.tk", false, true, false, true, false),
+            TextReplacement("bluesmods.com", "aliucord.tk", false, true, true, false, false),
         )
         public fun emptyRule(): TextReplacement {
             return TextReplacement("", "", false, true, true, true, true)
